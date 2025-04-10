4eeb07a647740047c323a609e2b177f145e65b48
pglx <2824179595@qq.com>
2025-04-10 10:38:36 +0800

    complete return button

--- app/components/basic-ui/return-top-button.tsx
+++ app/components/basic-ui/return-top-button.tsx
@@ -3,6 +3,7 @@ import { ArrowIcon } from "@components/icons/arrow-icon"
 import { cn } from "@lib/utils"
 import { RingProgress } from "@components/icons/ring-progress"
 import React,{ useEffect,useState } from "react"
+import { motion, AnimatePresence } from "framer-motion";
 
 interface ReturnTopButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
@@ -34,17 +35,22 @@ export const ReturnTopButton = React.forwardRef<HTMLButtonElement, ReturnTopButt
     )
     //渲染
     return (
-      <button
-      ref={ref}
-      onClick={handleClick}
-      className={cn("group fixed cursor-pointer bottom-10 right-10", className)}
-      {...props}
-    >
-      <div className="relative">
-        <RingProgress angle={1.1*angle} color="#0ea5e9"/>
-        <ArrowIcon className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rotate-90" />
-      </div>
-    </button>
+      <AnimatePresence>
+        {angle>30&&(<motion.button 
+        ref={ref}
+        onClick={handleClick}
+        className={cn("group fixed cursor-pointer bottom-10 right-10", className)}
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{duration:0.3}}
+        exit={{opacity:0,scale:0.5,transition:{duration:0.3}}}
+        >
+          <div className="relative">
+            <RingProgress angle={1.1*angle} color="#0ea5e9"/>
+            <ArrowIcon className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rotate-90" />
+          </div>
+        </motion.button>)}
+      </AnimatePresence>
     );
   }
 );
